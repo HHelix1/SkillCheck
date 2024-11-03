25969e166e382ae9e86e194e2fc5faf2f209d443
HHelix1 <127223130+HHelix1@users.noreply.github.com>
2024-11-03 17:56:50 +0300

    Update ArrayTests.cs

--- SkillCheck/SkillCheck.Tests/Intern/EasyLevel (required)/ArrayTests.cs
+++ SkillCheck/SkillCheck.Tests/Intern/EasyLevel (required)/ArrayTests.cs
@@ -7,22 +7,22 @@
          */
 
         //[Fact]
-        //public void Find_max_number_in_array_and_returns_max_value()
-        //{
-        //    int result = ArrayHelper.Max(new int[] { 1, 5, 3, 9, 2 });
-        //    Assert.Equal(9, result); // Ожидается 9 как максимальное число
-        //}
+        public void Find_max_number_in_array_and_returns_max_value()
+        {
+            int result = ArrayHelper.Max(new int[] { 1, 5, 3, 9, 2 });
+            Assert.Equal(9, result); // Ожидается 9 как максимальное число
+        }
 
         /*
          * Тест на сортировку массива
          */
 
         //[Fact]
-        //public void Sort_array_and_returns_sorted_array()
-        //{
-        //    int[] result = ArrayHelper.Sort(new int[] { 3, 1, 4, 2 });
-        //    Assert.Equal(new int[] { 1, 2, 3, 4 }, result); // Ожидается отсортированный массив
-        //}
+        public void Sort_array_and_returns_sorted_array()
+        {
+            int[] result = ArrayHelper.Sort(new int[] { 3, 1, 4, 2 });
+            Assert.Equal(new int[] { 1, 2, 3, 4 }, result); // Ожидается отсортированный массив
+        }
     }
 }
 namespace SkillCheck
@@ -46,7 +46,7 @@ public class ArrayHelper
     public static int[] Sort(int[] array)
         {
             if (array == null || array.Length == 0)
-                throw new ArgumentException("Array must not be null or empty");
+                throw new ArgumentException("Массив не должен быть равен null");
 
             int[] sortedArray = (int[])array.Clone();
             int n = sortedArray.Length;
